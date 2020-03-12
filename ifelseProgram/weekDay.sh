64ef33ce86d2daed1e6d2902027fe8f5f3d7589c
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-12 18:49:09 +0530

    Added:-Display the week day

---
+++ ifelseProgram/weekDay.sh
@@ -0,0 +1,24 @@
+read -p "enter number 1 - 7= " x
+
+if [ $x -eq 1 ]
+ then 
+	echo sunday
+elif [ $x -eq 2 ]
+then 
+	echo monday
+elif [ $x -eq 3 ]
+then 
+   echo tuseday
+elif [ $x -eq 4 ]
+then 
+   echo wensday
+elif [ $x -eq 5 ]
+then 
+   echo thursday
+elif [ $x -eq 6 ]
+then 
+   echo frieday
+else
+ echo saturday
+fi
+
