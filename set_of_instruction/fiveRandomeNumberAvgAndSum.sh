0d4c37c19b92e5ab014b62da9c501870626e5ac5
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-13 15:04:14 +0530

    Added:-add sum and avg of 5 randome two digit number

--- set_of_instruction/fiveRandomeNumberAvgAndSum.sh
+++ set_of_instruction/fiveRandomeNumberAvgAndSum.sh
@@ -1,15 +1,14 @@
 #!/bin/bash -x
 
-randomeNumber1=$((RANDOM%100));
-randomeNumber2=$((RANDOM%100));
-randomeNumber3=$((RANDOM%100));
-randomeNumber4=$((RANDOM%100));
-randomeNumber5=$((RANDOM%100));
-	if [ $randomeNumber1>10 ] && [ $randomeNumber2>10 ] && [ $randomeNumber3>10 ] && [ $randomeNumber4>10 ] && [ $randomeNumber5>10 ];
-		then
+randomeNumber1=$((RANDOM%90 +10));
+randomeNumber2=$((RANDOM%90 +10));
+randomeNumber3=$((RANDOM%90 +10));
+randomeNumber4=$((RANDOM%90 +10));
+randomeNumber5=$((RANDOM%90 +10));
+	
 			sum=$(( $randomeNumber1 + $randomeNumber2 + $randomeNumber3 + $randomeNumber4 + $randomeNumber5 ));
 			echo $sum
    		avg=$(( $sum / 5 ));
 			echo $avg
-		fi
+		
 
