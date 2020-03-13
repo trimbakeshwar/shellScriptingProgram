161a3f72618b29ac16d8826daa821e3098286196
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-13 17:21:43 +0530

    Added:-Number and Display the week day

---
+++ case_statment/digitToWeek.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+read -p "enter day " x
+case $x in
+	 0)
+         echo sunday ;;
+    1)
+         echo monday ;;
+    2)
+         echo tusday ;;
+    3)
+         echo wensday ;;
+    4)
+         echo thursday ;;
+    5)
+         echo friday ;;
+    6)
+         echo saturday ;;
+esac
