b8c86bca3dc6b8fd9715df04ced6a75f9ebe0d2a
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-13 13:05:04 +0530

    Added:- print week day on the basis date

--- set_of_instruction/printDayOfWeek.sh
+++ set_of_instruction/printDayOfWeek.sh
@@ -14,26 +14,27 @@ else
  		x=$(($y0 + $y0/4 - $y0 / 100 + $y0 / 400)) ;
  		m0=$(($m + 12 * ((14 - $m) / 12) - 2)) ;
  		d0=$((($d + $x + 31*$m0 / 12) % 7)) ;
-			if [ $d0==0 ]
+fi
+			if [ $d0 -eq 0 ]
 				then
 					echo sunday
-			elif [ $d0==1 ]
+			elif [ $d0 -eq 1 ]
 				then
 					echo monday
-			elif [ $d0==2 ]
+			elif [ $d0 -eq 2 ]
    			then
       			echo tuesday
-			elif [ $d0==3 ]
+			elif [ $d0 -eq 3 ]
    			then
       			echo wensday
-			elif [ $d0==4 ]
+			elif [ $d0 -eq 4 ]
    			then
       			echo thursday
-			elif [ $d0==5 ]
+			elif [ $d0 -eq 5 ]
    			then
       			echo friday
-			elif [ $d0==6 ]
+			elif [ $d0 -eq 6 ]
    			then
       			echo saturday
 				fi
-	fi
+	
