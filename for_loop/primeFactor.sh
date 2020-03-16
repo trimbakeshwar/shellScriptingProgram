621eaa63c54e69bcc5e28d6989318fe9a7a97202
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-16 17:36:16 +0530

    commited:-prime factors

--- for_loop/primeFactor.sh
+++ for_loop/primeFactor.sh
@@ -1,20 +1,10 @@
 #!/bin/bash -x
-read -p "enter number " num
-	for (( i=2; i<$num; i++ ))
-		do
-			if (( $num%$i==0 ))
-				then
-					count=0
-					for (( j=1; j<=$i; j++ ))
-						do
-							if (( $i%$j==0 ))
-								then
-									(( count++ ))
-							fi
-					done
-				if [ $count==2 ]
-					then
-						echo $i
-				fi
-			fi
-	done
+read -p "enter number" num
+for (( i=2; i<=$num; i++ ))
+do
+   while (( $num%$i==0 ))
+   do
+       echo $i
+      num=$num/$i
+   done
+done
