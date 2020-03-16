f1e8294279305a11390240a3b02ef90541407f85
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-16 17:56:39 +0530

    Added:-add prime factor in array

---
+++ arrays/primeFctor.sh
@@ -0,0 +1,18 @@
+#!/bin/bash  
+read -p "enter number" num
+count=0
+function primeFactor()
+{
+count=0
+for (( i=2; i<=$num; i++ ))
+do
+   while (( $num%$i==0 ))
+   do
+       echo $i
+      array[((count++))]=$i
+      num=$num/$i
+   done
+done
+}
+result="$(primeFactor $(($num)) )"
+echo $result
