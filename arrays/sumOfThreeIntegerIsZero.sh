b214d77c9b48ae74ea352a88dd66b5a64ae7cec3
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-16 17:58:16 +0530

    Added:- identify sum of three integer is zero

---
+++ arrays/sumOfThreeIntegerIsZero.sh
@@ -0,0 +1,32 @@
+#!/bin/bash 
+read -p "enter size" size
+
+function checkSumOfElement()
+{
+for (( i=0; i<$size; i++ ))
+do
+   for (( j=$i+1; j<$size; j++ ))
+   do
+      for (( k=$j+1; k<$size; k++ ))
+      do
+      sum=$(( ${array[i]}+${array[j]}+${array[k]} ))
+      if [ $sum -eq 0 ]
+      then
+         echo "${array[i]} ${array[j]} ${array[k]}"
+      fi
+      done
+   done
+done
+}
+
+
+count=0
+while [ $count -lt $size ]
+do
+	read -p "enter element" element
+	array[((count++))]=$element
+done
+echo ${array[@]}
+
+checkSumOfElement ${array[@]}
+
