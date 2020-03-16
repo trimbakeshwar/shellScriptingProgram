71959ec792fe1a823bb30bb7076b723827e1530d
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-16 17:47:30 +0530

    Added:-check number is prime check then this number is palindrome

---
+++ functiond/check_Number_Is_Prime_Then_Palindrome_Then_This_Palindrome_Is_Prime_Or_Not.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -x
+
+function checkPrime(){
+count=0
+for (( j=1; j<=$num; j++ ))
+do
+	if (( $num%$j==0 ))
+	then
+		(( count++ ))
+	fi
+done
+	if [ $count -eq 2 ]
+	then
+	 "$( checkPalindrome $(($num)) )"
+	else
+		echo "not prime"
+	fi
+}
+
+
+function checkPalindrome(){
+sum=0
+temp=0
+temp=$num
+while (( $num!=0 ))
+   do
+      rem=$num%10
+      (( sum=$sum*10+$rem ))
+      num=$num/10
+   done
+if [ $temp -eq $sum ]
+   then
+     echo "this number is palindrome and prime"
+   else
+      echo "number is prime but not palindrome"
+   fi
+}
+
+read -p "enter number" num
+"$( checkPrime $(($num)) )"
+
