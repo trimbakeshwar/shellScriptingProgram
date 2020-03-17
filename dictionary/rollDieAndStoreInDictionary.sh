1dd434c0b74f9a2fc3d9742496848d737e35682f
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-17 17:28:08 +0530

    Added:-produce randome dice number and store in dictionary

--- dictionary/rollDieAndStoreInDictionary.sh
+++ dictionary/rollDieAndStoreInDictionary.sh
@@ -1,17 +1,17 @@
 #!/bin/bash 
 declare -A result
-rollDie=1
+rollDie=0
 nax=0
 while [[ result[$rollDie] -le 9 ]]
 do
-rollDie=$((RANDOM%6+1))
-result[$rollDie]=$((${result[$rollDie]}+1))
+	rollDie=$((RANDOM%6+1))
+	result[$rollDie]=$((${result[$rollDie]}+1))
 if [[ result[$rollDie] -eq 10 ]]
 then
 	echo "max count is present on this $rollDie key"
 fi
 done
-echo "${!result[@]}     ${result[@]}"
+	echo "${!result[@]}     ${result[@]}"
 min=${result[1]}
 
 for (( i=1; i<=6; i++ ))
@@ -21,12 +21,11 @@ do
 		min=${result[$i]}
 	fi
 done
-echo $min
 for (( i=1; i<=6; i++ ))
 do
 	if [[ $min -eq ${result[$i]} ]]
 	then
-		echo "$i
+		echo "min count on this $i key"
 	fi
 done
 
