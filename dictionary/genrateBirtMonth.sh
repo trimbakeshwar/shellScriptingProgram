b7762b30acf9102380902f638ae54cccb1882123
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-17 17:14:02 +0530

    Added:- genrate months and store same birt month in array

---
+++ dictionary/genrateBirtMonth.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+function checkBirthMonth()
+{
+declare -A result
+count=0
+month=0
+while [[ $count -le 50 ]]
+do
+month=$((RANDOM%12 +1))
+result[$month]=$((${result[$month]}+1))
+	((count++))
+done
+ 	echo "${!result[@]}     ${result[@]}"
+ }
+read -p "enter year" year
+if [[ $year -eq 92 ]] || [[ $year -eq 93 ]]
+then
+ checkBirthMonth 
+fi
+
