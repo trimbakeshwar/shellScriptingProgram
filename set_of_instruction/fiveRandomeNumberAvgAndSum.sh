8bc4fb977762d4689f0cd79c0659b20ae1efc779
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-20 17:19:21 +0530

    modified:-five randome number avg and sum

--- set_of_instruction/fiveRandomeNumberAvgAndSum.sh
+++ set_of_instruction/fiveRandomeNumberAvgAndSum.sh
@@ -6,9 +6,9 @@ randomeNumber3=$((RANDOM%90 +10));
 randomeNumber4=$((RANDOM%90 +10));
 randomeNumber5=$((RANDOM%90 +10));
 	
-			sum=$(( $randomeNumber1 + $randomeNumber2 + $randomeNumber3 + $randomeNumber4 + $randomeNumber5 ));
-			echo $sum
-   		avg=$(( $sum / 5 ));
-			echo $avg
+	sum=$(( $randomeNumber1 + $randomeNumber2 + $randomeNumber3 + $randomeNumber4 + $randomeNumber5 ));
+	echo $sum
+   avg=$(( $sum / 5 ));
+	echo $avg
 		
 
