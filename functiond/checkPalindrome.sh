222ebf06d353ddfafbecf424e8d7ffab16a4931f
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-20 16:18:40 +0530

    modified:-check palindrome

--- functiond/checkPalindrome.sh
+++ functiond/checkPalindrome.sh
@@ -5,17 +5,17 @@ sum=0
 temp=0
 temp=$num
 while (( $num!=0 ))
-	do
-		rem=$num%10
-		(( sum=$sum*10+$rem ))
-		num=$num/10
-	done
+do
+	rem=$num%10
+	(( sum=$sum*10+$rem ))
+	num=$num/10
+done
 if [ $temp -eq $sum ]
-	then
-		echo $sum
-	else
-		echo "number is not palindrome"
-	fi
+then
+	echo $sum
+else
+	echo "number is not palindrome"
+fi
 }
 read -p "enter number" num
 result1="$( checkPalindrome $(($num)) )"
