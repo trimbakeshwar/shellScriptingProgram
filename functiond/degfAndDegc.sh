2402315911aa0f5a0ff657f62225dfcddaae17b7
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-20 16:21:44 +0530

    modified:-degree to c

--- functiond/degfAndDegc.sh
+++ functiond/degfAndDegc.sh
@@ -4,34 +4,31 @@ read -p "enter dgree " degree
 #read -p "enter 1 for degree to F and 2 for degree to c" choice
 
 function degreeToF(){
-
-degf=`echo "($degree*9/5)+32" | bc -l`
-echo $degf
+	degf=`echo "($degree*9/5)+32" | bc -l`
+	echo $degf
 }
 
 function degreeToC(){
-
-degC=`echo "(($degree-32)*5/9)" | bc -l`
-echo $degC
+	degC=`echo "(($degree-32)*5/9)" | bc -l`
+	echo $degC
 }
 read -p "enter 1 for degree to F and 2 for degree to c" choice
-
 case $choice in
-1)
-if [ $degree -gt 0 ] && [ $degree -lt 32 ]
-then
-	result="$(degreeToF $((degree)) )"
-	echo $result
-fi
-;;
-2)
-if [ $degree -gt 100 ] && [ $degree -lt 212 ]
-then
-	result2="$(degreeToC $((degree)) )"
-   echo $result2
-fi
-;;
+	1)
+	if [ $degree -gt 0 ] && [ $degree -lt 32 ]
+	then
+		result="$(degreeToF $((degree)) )"
+		echo $result
+	fi
+	;;
+	2)
+	if [ $degree -gt 100 ] && [ $degree -lt 212 ]
+	then
+		result2="$(degreeToC $((degree)) )"
+   	echo $result2
+	fi
+	;;
 esac
 
 
- 
+
