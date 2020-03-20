3152e8a291f85ffd58c0a9118a2f3433b7a5dd50
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-20 16:55:09 +0530

    modified:-month and day between march to june

--- if_select_statment/monthAndDayBetweenMarchAmdJun.sh
+++ if_select_statment/monthAndDayBetweenMarchAmdJun.sh
@@ -8,29 +8,29 @@ may=5;
 jun=6;
 
 if [ $m -eq $march ] 
- then 
-		if [ $d -gt 20 ] && [ $d -le 31 ]
-		 then
-			 echo "true" 
-		else
-			 echo "false"
-		fi 
+then 
+	if [ $d -gt 20 ] && [ $d -le 31 ]
+	then
+		 echo "true" 
+	else
+		 echo "false"
+	fi 
 elif [ $m -eq  $jun ]
+then
+	if [ $d -le 20 ] && [ $d -ge 1 ]
 	then
-		if [ $d -le 20 ] && [ $d -ge 1 ]
-		then
-			echo "true"
-		else
-			echo "fals"
-		fi
+		echo "true"
+	else
+		echo "fals"
+	fi
 elif [ $m -gt 3 ] && [ $m -lt 6 ]
-	then
-		if [ $m -ge 1 ] && [ $m -le 31 ]
-   	then
-			echo "true"
-		else
-			echo "false"
-		fi
+then
+	if [ $m -ge 1 ] && [ $m -le 31 ]
+   then
+		echo "true"
+	else
+		echo "false"
+	fi
 else
 		echo "please eter valid input"
 fi
