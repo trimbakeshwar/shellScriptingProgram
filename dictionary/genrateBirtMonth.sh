208808e3a0bbdb2fccb5e3bc843ed62f71e5f84a
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-20 15:26:21 +0530

    modified:-genrate birth month

--- dictionary/genrateBirtMonth.sh
+++ dictionary/genrateBirtMonth.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
+declare -A result
 function checkBirthMonth()
 {
-declare -A result
 count=0
 month=0
 while [[ $count -le 50 ]]
 do
-month=$((RANDOM%12 +1))
-result[$month]=$((${result[$month]}+1))
+	month=$((RANDOM%12 +1))
+	result[$month]=$((${result[$month]}+1))
 	((count++))
 done
  	echo "${!result[@]}     ${result[@]}"
@@ -15,6 +15,6 @@ done
 read -p "enter year" year
 if [[ $year -eq 92 ]] || [[ $year -eq 93 ]]
 then
- checkBirthMonth 
+	checkBirthMonth 
 fi
 
