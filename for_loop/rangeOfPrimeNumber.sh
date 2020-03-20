a2f87ff7f8c471039a6c5e06183616985c42295a
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-20 16:08:27 +0530

    modified:-rang of prime factor

--- for_loop/rangeOfPrimeNumber.sh
+++ for_loop/rangeOfPrimeNumber.sh
@@ -3,17 +3,17 @@ read -p "enter start range" start
 read -p "enter end range" end
 
 for (( i=$start; i<=$end; i++ ))
-	do
+do
 count=0
-		for (( j=1; j<=$i; j++ ))
-			do
-				if (( $i%$j==0 ))
-					then
-						(( count++ ))
-					fi
-			done
-	if [ $count -eq 2 ]
+	for (( j=1; j<=$i; j++ ))
+	do
+		if (( $i%$j==0 ))
 		then
-			echo $i
+			(( count++ ))
 		fi
 	done
+	if [ $count -eq 2 ]
+	then
+		echo $i
+	fi
+done
