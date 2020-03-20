aab5c5842b2db15c8474a8af9fcfdd22af8750cf
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-20 15:47:22 +0530

    modified:-harmonic number

--- for_loop/harmonicNumber.sh
+++ for_loop/harmonicNumber.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 read -p "enter nth number" n
 harmonic=1;
-	for (( count=2; count<=$n; count++ ))
-		do
-			harmonic=`echo "$harmonic+(1/$count)" | bc -l`
-			echo $harmonic
+for (( count=2; count<=$n; count++ ))
+do
+	harmonic=`echo "$harmonic+(1/$count)" | bc -l`
+	echo $harmonic
 done
